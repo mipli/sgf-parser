fac9687f59fd683c3d99d3cb8ccfd147c7435958
Michael A. Plikk <michael@aptoma.com>
2019-04-13 23:13:57 +0200

    Cleaned up token parsing code

--- src/token.rs
+++ src/token.rs
@@ -55,143 +55,122 @@ impl SgfToken {
             .chars()
             .filter(|c| c.is_uppercase())
             .collect::<String>();
-        match ident.as_ref() {
+        let token: Option<SgfToken> = match ident.as_ref() {
             "LB" => {
-                if value.len() >= 4 {
-                    let (coord, label) = value.split_at(2);
-                    if label.len() >= 2 {
-                        if let Ok(coordinate) = str_to_coordinates(coord) {
-                            return SgfToken::Label{
-                                label: label[1..].to_string(),
-                                coordinate,
-                            };
+                split_label_text(value).and_then(|(coord, label)| {
+                    str_to_coordinates(coord).ok().map(|coordinate| {
+                        SgfToken::Label{
+                            label: label[1..].to_string(),
+                            coordinate,
                         }
-                    }
-                }
-                SgfToken::Invalid((base_ident.to_string(), value.to_string()))
+                    })
+
+                })
             },
             "SQ" => {
-                if let Ok(coordinate) = str_to_coordinates(value) {
+                str_to_coordinates(value).ok().map(|coordinate| {
                     SgfToken::Square{
                         coordinate,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             },
             "TR" => {
-                if let Ok(coordinate) = str_to_coordinates(value) {
+                str_to_coordinates(value).ok().map(|coordinate| {
                     SgfToken::Triangle{
                         coordinate,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             },
             "AB" => {
-                if let Ok(coordinate) = str_to_coordinates(value) {
+                str_to_coordinates(value).ok().map(|coordinate| {
                     SgfToken::Add{
                         color: Color::Black,
                         coordinate,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             },
             "B" => {
-                if let Ok(coordinate) = str_to_coordinates(value) {
+                str_to_coordinates(value).ok().map(|coordinate| {
                     SgfToken::Move{
                         color: Color::Black,
                         coordinate,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             },
             "BL" => {
-                if let Ok(time) = value.parse() {
+                value.parse().ok().map(|time| {
                     SgfToken::Time {
                         color: Color::Black,
                         time,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             },
-            "PB" => SgfToken::PlayerName {
+            "PB" => Some(SgfToken::PlayerName {
                 color: Color::Black,
                 name: value.to_string(),
-            },
-            "BR" => SgfToken::PlayerRank {
+            }),
+            "BR" => Some(SgfToken::PlayerRank {
                 color: Color::Black,
                 rank: value.to_string(),
-            },
+            }),
             "AW" => {
-                if let Ok(coordinate) = str_to_coordinates(value) {
+                str_to_coordinates(value).ok().map(|coordinate| {
                     SgfToken::Add{
                         color: Color::White,
                         coordinate,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             }
             "W" => {
-                if let Ok(coordinate) = str_to_coordinates(value) {
-                    SgfToken::Move {
+                str_to_coordinates(value).ok().map(|coordinate| {
+                    SgfToken::Move{
                         color: Color::White,
                         coordinate,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             }
             "WL" => {
-                if let Ok(time) = value.parse() {
+                value.parse().ok().map(|time| {
                     SgfToken::Time {
                         color: Color::White,
                         time,
                     }
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             }
-            "PW" => SgfToken::PlayerName {
+            "PW" => Some(SgfToken::PlayerName {
                 color: Color::White,
                 name: value.to_string(),
-            },
-            "WR" => SgfToken::PlayerRank {
+            }),
+            "WR" => Some(SgfToken::PlayerRank {
                 color: Color::White,
                 rank: value.to_string(),
-            },
+            }),
             "KM" => {
-                if let Ok(komi) = value.parse() {
+                value.parse().ok().map(|komi| {
                     SgfToken::Komi(komi)
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             }
             "SZ" => {
-                if let Ok(size) = value.parse() {
+                value.parse().ok().map(|size| {
                     SgfToken::Size(size)
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             }
             "TM" => {
-                if let Ok(time) = value.parse() {
+                value.parse().ok().map(|time| {
                     SgfToken::TimeLimit(time)
-                } else {
-                    SgfToken::Invalid((base_ident.to_string(), value.to_string()))
-                }
+                })
             }
-            "EV" => SgfToken::Event(value.to_string()),
-            "C" => SgfToken::Comment(value.to_string()),
-            "GN" => SgfToken::GameName(value.to_string()),
-            "CR" => SgfToken::Copyright(value.to_string()),
-            "DT" => SgfToken::Date(value.to_string()),
-            "PC" => SgfToken::Place(value.to_string()),
-            _ => SgfToken::Unknown((base_ident.to_string(), value.to_string())),
+            "EV" => Some(SgfToken::Event(value.to_string())),
+            "C" => Some(SgfToken::Comment(value.to_string())),
+            "GN" => Some(SgfToken::GameName(value.to_string())),
+            "CR" => Some(SgfToken::Copyright(value.to_string())),
+            "DT" => Some(SgfToken::Date(value.to_string())),
+            "PC" => Some(SgfToken::Place(value.to_string())),
+            _ => Some(SgfToken::Unknown((base_ident.to_string(), value.to_string()))),
+        };
+        match token {
+            Some(token) => token,
+            _ => SgfToken::Invalid((base_ident.to_string(), value.to_string()))
         }
     }
 }
@@ -281,6 +260,16 @@ fn coordinate_to_str(coordinate: (u8, u8)) -> String {
     let x = conv(coordinate.0) as char;
     let y = conv(coordinate.1) as char;
     [x, y].iter().collect()
+}
+
+/// If possible, splits a label text into coordinate and label pair
+fn split_label_text(input: &str) -> Option<(&str, &str)> {
+    if input.len() >= 4 {
+        Some(input.split_at(2))
+    } else {
+        None
+    }
+
 }
 
 /// Converts a string describing goban coordinates to numeric coordinates
