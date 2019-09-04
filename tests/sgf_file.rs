eef5215826f0b1f4d673f11947ae17829df21943
Samuel Batissou <samuelbatissou@gmail.com>
2019-09-04 17:50:22 +0200

    cargo fmt runnes

--- tests/sgf_file.rs
+++ tests/sgf_file.rs
@@ -2,8 +2,6 @@
 mod sgf_files_test {
     #[test]
     fn parse_sfg() {
-        let _g  =
-            sgf_parser::parse(include_str!("sgf/ShusakuvsInseki.sgf"))
-                .unwrap();
+        let _g = sgf_parser::parse(include_str!("sgf/ShusakuvsInseki.sgf")).unwrap();
     }
 }
